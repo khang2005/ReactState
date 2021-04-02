f1915349e79c538d43068c5d288fb0400013e5ab
Khang Tran <khangtran082005@gmail.com>
2021-04-02 16:35:12 -0700

    using Webpack Manually

--- remove.js
+++
@@ -1,6 +0,0 @@
-db.getCollectionNames().forEach( function(collection_name) { 
-    if (collection_name.indexOf("system.") == -1) {
-        print ( ["Removing: ", db[collection_name].count({}), " documents from ", collection_name].join('') );
-        db[collection_name].remove({}); 
-    }
-});
