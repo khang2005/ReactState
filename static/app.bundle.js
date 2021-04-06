e9b683147572eb6a2c7df3696631f48031a047a6
Khang Tran <khangtran082005@gmail.com>
2021-04-05 20:22:32 -0700

    Transform and bundle

--- static/app.bundle.js
+++ static/app.bundle.js
@@ -42,42 +42,46 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
 
+	var _IssueList = __webpack_require__(1);
+
+	var _IssueList2 = _interopRequireDefault(_IssueList);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var contentNode = document.getElementById('contents');
+	ReactDOM.render(React.createElement(_IssueList2.default, null), contentNode); // Render the component inside the content Node
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _IssueAdd = __webpack_require__(2);
+
+	var _IssueAdd2 = _interopRequireDefault(_IssueAdd);
+
+	var _IssueFilter = __webpack_require__(3);
+
+	var _IssueFilter2 = _interopRequireDefault(_IssueFilter);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var contentNode = document.getElementById('contents');
-
-	var IssueFilter = function (_React$Component) {
-	  _inherits(IssueFilter, _React$Component);
-
-	  function IssueFilter() {
-	    _classCallCheck(this, IssueFilter);
-
-	    return _possibleConstructorReturn(this, (IssueFilter.__proto__ || Object.getPrototypeOf(IssueFilter)).apply(this, arguments));
-	  }
-
-	  _createClass(IssueFilter, [{
-	    key: 'render',
-	    value: function render() {
-	      return React.createElement(
-	        'div',
-	        null,
-	        'This is a placeholder for the Issue Filter.'
-	      );
-	    }
-	  }]);
-
-	  return IssueFilter;
-	}(React.Component);
 
 	var IssueRow = function IssueRow(props) {
 	  return React.createElement(
@@ -179,68 +183,18 @@
 	  );
 	}
 
-	var IssueAdd = function (_React$Component2) {
-	  _inherits(IssueAdd, _React$Component2);
-
-	  function IssueAdd() {
-	    _classCallCheck(this, IssueAdd);
-
-	    var _this2 = _possibleConstructorReturn(this, (IssueAdd.__proto__ || Object.getPrototypeOf(IssueAdd)).call(this));
-
-	    _this2.handleSubmit = _this2.handleSubmit.bind(_this2);
-	    return _this2;
-	  }
-
-	  _createClass(IssueAdd, [{
-	    key: 'handleSubmit',
-	    value: function handleSubmit(e) {
-	      e.preventDefault();
-	      var form = document.forms.issueAdd;
-	      this.props.createIssue({
-	        owner: form.owner.value,
-	        title: form.title.value,
-	        status: 'New',
-	        created: new Date()
-	      });
-	      // clear the form for the next input
-	      form.owner.value = "";form.title.value = "";
-	    }
-	  }, {
-	    key: 'render',
-	    value: function render() {
-	      return React.createElement(
-	        'div',
-	        null,
-	        React.createElement(
-	          'form',
-	          { name: 'issueAdd', onSubmit: this.handleSubmit },
-	          React.createElement('input', { type: 'text', name: 'owner', placeholder: 'Owner' }),
-	          React.createElement('input', { type: 'text', name: 'title', placeholder: 'Title' }),
-	          React.createElement(
-	            'button',
-	            null,
-	            'Add'
-	          )
-	        )
-	      );
-	    }
-	  }]);
-
-	  return IssueAdd;
-	}(React.Component);
-
-	var IssueList = function (_React$Component3) {
-	  _inherits(IssueList, _React$Component3);
+	var IssueList = function (_React$Component) {
+	  _inherits(IssueList, _React$Component);
 
 	  function IssueList() {
 	    _classCallCheck(this, IssueList);
 
-	    var _this3 = _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).call(this));
+	    var _this = _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).call(this));
 
-	    _this3.state = { issues: [] };
+	    _this.state = { issues: [] };
 
-	    _this3.createIssue = _this3.createIssue.bind(_this3);
-	    return _this3;
+	    _this.createIssue = _this.createIssue.bind(_this);
+	    return _this;
 	  }
 
 	  _createClass(IssueList, [{
@@ -251,7 +205,7 @@
 	  }, {
 	    key: 'loadData',
 	    value: function loadData() {
-	      var _this4 = this;
+	      var _this2 = this;
 
 	      fetch('/api/issues').then(function (response) {
 	        if (response.ok) {
@@ -261,7 +215,7 @@
 	              issue.created = new Date(issue.created);
 	              if (issue.completionDate) issue.completionDate = new Date(issue.completionDate);
 	            });
-	            _this4.setState({ issues: data.records });
+	            _this2.setState({ issues: data.records });
 	          });
 	        } else {
 	          response.json().then(function (error) {
@@ -275,7 +229,7 @@
 	  }, {
 	    key: 'createIssue',
 	    value: function createIssue(newIssue) {
-	      var _this5 = this;
+	      var _this3 = this;
 
 	      fetch('/api/issues', {
 	        method: 'POST',
@@ -286,8 +240,8 @@
 	          response.json().then(function (updatedIssue) {
 	            updatedIssue.created = new Date(updatedIssue.created);
 	            if (updatedIssue.completionDate) updatedIssue.completionDate = new Date(updatedIssue.completionDate);
-	            var newIssues = _this5.state.issues.concat(updatedIssue);
-	            _this5.setState({ issues: newIssues });
+	            var newIssues = _this3.state.issues.concat(updatedIssue);
+	            _this3.setState({ issues: newIssues });
 	          });
 	        } else {
 	          response.json().then(function (error) {
@@ -309,11 +263,11 @@
 	          null,
 	          'Issue Tracker'
 	        ),
-	        React.createElement(IssueFilter, null),
+	        React.createElement(_IssueFilter2.default, null),
 	        React.createElement('hr', null),
 	        React.createElement(IssueTable, { issues: this.state.issues }),
 	        React.createElement('hr', null),
-	        React.createElement(IssueAdd, { createIssue: this.createIssue })
+	        React.createElement(_IssueAdd2.default, { createIssue: this.createIssue })
 	      );
 	    }
 	  }]);
@@ -321,7 +275,120 @@
 	  return IssueList;
 	}(React.Component);
 
-	ReactDOM.render(React.createElement(IssueList, null), contentNode); // Render the component inside the content Node
+	exports.default = IssueList;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var IssueAdd = function (_React$Component) {
+	  _inherits(IssueAdd, _React$Component);
+
+	  function IssueAdd() {
+	    _classCallCheck(this, IssueAdd);
+
+	    var _this = _possibleConstructorReturn(this, (IssueAdd.__proto__ || Object.getPrototypeOf(IssueAdd)).call(this));
+
+	    _this.handleSubmit = _this.handleSubmit.bind(_this);
+	    return _this;
+	  }
+
+	  _createClass(IssueAdd, [{
+	    key: "handleSubmit",
+	    value: function handleSubmit(e) {
+	      e.preventDefault();
+	      var form = document.forms.issueAdd;
+	      this.props.createIssue({
+	        owner: form.owner.value,
+	        title: form.title.value,
+	        status: 'New',
+	        created: new Date()
+	      });
+	      // clear the form for the next input
+	      form.owner.value = "";form.title.value = "";
+	    }
+	  }, {
+	    key: "render",
+	    value: function render() {
+	      return React.createElement(
+	        "div",
+	        null,
+	        React.createElement(
+	          "form",
+	          { name: "issueAdd", onSubmit: this.handleSubmit },
+	          React.createElement("input", { type: "text", name: "owner", placeholder: "Owner" }),
+	          React.createElement("input", { type: "text", name: "title", placeholder: "Title" }),
+	          React.createElement(
+	            "button",
+	            null,
+	            "Add"
+	          )
+	        )
+	      );
+	    }
+	  }]);
+
+	  return IssueAdd;
+	}(React.Component);
+
+	exports.default = IssueAdd;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var IssueFilter = function (_React$Component) {
+	  _inherits(IssueFilter, _React$Component);
+
+	  function IssueFilter() {
+	    _classCallCheck(this, IssueFilter);
+
+	    return _possibleConstructorReturn(this, (IssueFilter.__proto__ || Object.getPrototypeOf(IssueFilter)).apply(this, arguments));
+	  }
+
+	  _createClass(IssueFilter, [{
+	    key: "render",
+	    value: function render() {
+	      return React.createElement(
+	        "div",
+	        null,
+	        "This is a placeholder for the Issue Filter."
+	      );
+	    }
+	  }]);
+
+	  return IssueFilter;
+	}(React.Component);
+
+	exports.default = IssueFilter;
 
 /***/ })
 /******/ ]);
